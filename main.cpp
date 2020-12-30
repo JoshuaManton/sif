66b5c7b9a86a3f2361ab90380f9189dcfc54229e
Joshua Mark Manton <joshuamarkk@gmail.com>
2020-12-30 14:12:37 -0800

    added a compilation timer and added /nologo to cl invocation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <cassert>
 
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 #include "common.h"
 #include "lexer.h"
 #include "parser.h"
@@ -36,10 +40,39 @@ TODO:
 -generate C instead of C++. I think the only thing from C++ I'm using is for the templated Static_Array for array-by-value semantics
 */
 
+struct Timer {
+    double frequency = {};
+};
+
+void init_timer(Timer *timer) {
+    LARGE_INTEGER large_integer_frequency = {};
+    assert(QueryPerformanceFrequency(&large_integer_frequency) != 0);
+    timer->frequency = large_integer_frequency.QuadPart/1000.0;
+}
+
+double query_timer(Timer *timer) {
+    LARGE_INTEGER large_integer_counter = {};
+    assert(QueryPerformanceCounter(&large_integer_counter) != 0);
+    return large_integer_counter.QuadPart / timer->frequency;
+}
+
 void main(int argc, char **argv) {
+    Timer timer = {};
+    init_timer(&timer);
+    double application_start_time = query_timer(&timer);
+
     if (argc < 3) {
-        printf("Usage:\n  sif <build|run> <file>\n");
+        printf("Usage:\n  sif <build|run> <file> [-show-timings]\n");
         return;
+    }
+
+    bool show_timings = false;
+
+    for (int i = 2; i < argc; i++) {
+        char *arg = argv[i];
+        if (strcmp(arg, "-show-timings") == 0) {
+            show_timings = true;
+        }
     }
 
     bool is_run   = strcmp(argv[1], "run")   == 0;
@@ -49,6 +82,8 @@ void main(int argc, char **argv) {
     init_parser();
     init_checker();
 
+    double parsing_start_time = query_timer(&timer);
+
     Ast_Block *global_scope = begin_parsing(argv[2]);
     if (!global_scope) {
         printf("There were errors.\n");
@@ -57,18 +92,38 @@ void main(int argc, char **argv) {
 
     add_global_declarations(global_scope);
 
+    double checking_start_time = query_timer(&timer);
+
     bool check_success = typecheck_global_scope(global_scope);
     if (!check_success) {
         printf("There were errors.\n");
         return;
     }
 
+    double codegen_start_time = query_timer(&timer);
+
     String_Builder c_code = generate_c_main_file(global_scope);
     write_entire_file("output.cpp", c_code.string());
 
-    if (system("cmd.exe /c \"cl output.cpp\"") != 0) {
-        printf("sif encountered an error when compiling C output. Exiting.\n");
+    double c_compile_start_time = query_timer(&timer);
+
+    if (system("cmd.exe /c \"cl output.cpp /nologo\"") != 0) {
+        printf("\nInternal compiler error: sif encountered an error when compiling C output. Exiting.\n");
         return;
+    }
+
+    double compilation_end_time = query_timer(&timer);
+
+    if (show_timings) {
+        printf("-----------------------------\n");
+        printf("|    sif compile timings    |\n");
+        printf("-----------------------------\n");
+        printf("Setup     time: %fs\n", (parsing_start_time   - application_start_time));
+        printf("Parse     time: %fs\n", (checking_start_time  - parsing_start_time));
+        printf("Check     time: %fs\n", (codegen_start_time   - checking_start_time));
+        printf("Codegen   time: %fs\n", (c_compile_start_time - codegen_start_time));
+        printf("C compile time: %fs\n", (compilation_end_time - c_compile_start_time));
+        printf("Total     time: %fs\n", (compilation_end_time - application_start_time));
     }
 
     if (is_run) {
