edf420fea82b588b1b0b960712b6e9be068a1b88
Joshua Mark Manton <joshuamarkk@gmail.com>
2020-12-30 21:44:40 -0800

    moved code into src/ directory

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist "bin" mkdir bin
-cl /MP /Zi /Od /Fd /Iexternal main.cpp lexer.cpp parser.cpp checker.cpp c_backend.cpp basic.cpp user32.lib ole32.lib oleaut32.lib /EHsc /link /INCREMENTAL:no /OUT:bin/sif.exe /DEBUG
+cl /MP /Zi /Od /Fd /Iexternal src/main.cpp src/lexer.cpp src/parser.cpp src/checker.cpp src/c_backend.cpp src/basic.cpp user32.lib ole32.lib oleaut32.lib /EHsc /link /INCREMENTAL:no /OUT:bin/sif.exe /DEBUG
