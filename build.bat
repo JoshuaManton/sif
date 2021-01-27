1422d87c91a66cd1d5a2f2c61626526dd0018821
bumbread <thebumboni@gmail.com>
2021-01-28 08:46:43 +1100

    `rm` is not a windows command.
    Also made the build script output a little bit less annoying.

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 
-rm sif.exe
+del sif.exe
 
 set optimizations=/Od
 if "%1"=="release" (
     set optimizations=/O2
 )
 
-cl /MP /Zi /Fd %optimizations% /Iexternal src/main.cpp src/lexer.cpp src/parser.cpp src/checker.cpp src/c_backend.cpp src/basic.cpp src/os_windows.cpp src/common.cpp src/ir.cpp user32.lib advapi32.lib ole32.lib oleaut32.lib /EHsc /link /INCREMENTAL:no /OUT:sif.exe /DEBUG
-del *.obj
+cl /nologo /MP /Zi /Fd %optimizations% /Iexternal src/main.cpp src/lexer.cpp src/parser.cpp src/checker.cpp src/c_backend.cpp src/basic.cpp src/os_windows.cpp src/common.cpp src/ir.cpp user32.lib advapi32.lib ole32.lib oleaut32.lib /EHsc /link /INCREMENTAL:no /OUT:sif.exe /DEBUG
+del *.obj >nul 2>nul
