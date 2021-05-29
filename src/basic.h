b93899a7db7481e0f697b180abbc4abe11adf6f1
bumbread <yyakut.ac@gmail.com>
2021-05-29 22:09:17 +1100

    Use standard type definitions from STDINT.H
    Rationale: It may be desirable to compile this compiler
      using a different compiler from MSVC, e.g. to get
      better optimizations.
    
      Different compilers have differently-sized types,
      making the code non-compilable.
    
      Using STDINT.H types will ensure a bett-"er"
      compilability with such compilers without changing the
      codebase and the assumptions about the sizes of types.

--- src/basic.h
+++ src/basic.h
@@ -6,29 +6,26 @@
 #include <math.h>
 #include <float.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "spinlock.h"
 
-typedef unsigned char      byte; static_assert(sizeof(byte) == 1, "byte size was not 1");
-typedef unsigned char      u8;   static_assert(sizeof(u8)   == 1, "u8 size was not 1");
-typedef unsigned short     u16;  static_assert(sizeof(u16)  == 2, "u16 size was not 2");
-typedef unsigned int       u32;  static_assert(sizeof(u32)  == 4, "u32 size was not 4");
-typedef unsigned long long u64;  static_assert(sizeof(u64)  == 8, "u64 size was not 8");
+typedef uint8_t byte;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef u32 uint;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
 
-typedef u32 uint; static_assert(sizeof(uint)  == 4, "uint size was not 4");
-
-typedef char      i8;   static_assert(sizeof(i8)   == 1, "i8 size was not 1");
-typedef short     i16;  static_assert(sizeof(i16)  == 2, "i16 size was not 2");
-typedef int       i32;  static_assert(sizeof(i32)  == 4, "i32 size was not 4");
-typedef long long i64;  static_assert(sizeof(i64)  == 8, "i64 size was not 8");
-
+static_assert(sizeof(float)  == 4, "float size was not 4");
+static_assert(sizeof(double)  == 8, "double size was not 8");
 static_assert(sizeof(int)  == 4, "int size was not 4");
-
-typedef float  f32; static_assert(sizeof(f32) == 4, "f32 size was not 4");
-typedef double f64; static_assert(sizeof(f64) == 8, "f64 size was not 8");
-
-static_assert(sizeof(float) == 4, "float size was not 4");
-
 static_assert(sizeof(void *) == 8, "void * size was not 8");
 
 // #ifndef ASSERT
